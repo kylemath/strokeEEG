3f454ae1694322cecdabee216c4fd65626987444
Kyle Mathewson <kylemath@gmail.com>
2022-11-16 23:39:45 -0700

    loading into mne

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import mne
+import numpy as np
+import time
 pd.options.display.float_format = '{:.9f}'.format
 
 
 # for isub in [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]:
 # missing files = [3, 8, 12, 15, 16, 17, 19]
 # 
-for isub in [1, 2, 4, 5, 6, 7, 9, 10, 11, 13, 14, 18, 20, 21, 22, 23, 24, 25, 26, 27]:	
+# for isub in [1, 2, 4, 5, 6, 7, 9, 10, 11, 13, 14, 18, 20, 21, 22, 23, 24, 25, 26, 27]:
+for isub in [27]:
 	substr = str(isub).zfill(2)
 	print('______________')
 	print('Subject #: ' + substr)
 	print('______________')
 
-	fig, axs = plt.subplots(5+3+3, 3, figsize=(20,12))
+	# fig, axs = plt.subplots(5+3+3, 3, figsize=(20,12))
 
 
 	for isesh in range(1,4):
@@ -41,8 +44,30 @@ for isub in [1, 2, 4, 5, 6, 7, 9, 10, 11, 13, 14, 18, 20, 21, 22, 23, 24, 25, 26
 			ACCchannels = df.columns[9:12].tolist()
 			GYROchannels = df.columns[14:17].tolist()
 		
+
+		#create mne raw object from arrays
+		#info object 
 		# print(EEGchannels, ACCchannels, GYROchannels)
 
+		intervals = df['timestamps'].diff()
+		srate = round(1/intervals.mean())
+		ch_names = ['TP9', 'AF7', 'AF8', 'TP10', 'Wrist', 'X_x', 'Y_x', 'Z_x', 'X_y', 'Y_y', 'Z_y']
+		ch_types = ['eeg'] * 4 + ['ecg'] + ['bio'] * 6
+		info = mne.create_info(ch_names, ch_types=ch_types, sfreq=srate)
+		info.set_montage('standard_1020')
+
+
+
+		print(info)
+
+		df = df.drop(['index_x', 'timestamps',  'Marker0_x', 'index_y', 'Marker0_y', 'index', 'Marker0'], axis=1)
+		data = df.to_numpy().transpose()
+
+		rawData = mne.io.RawArray(data, info)
+		print(rawData)
+		rawData.plot(show_scrollbars=False, show_scalebars=False, block=True)
+		rawSpectra = rawData.compute_psd()
+		rawSpectra.plot()
 		##
 		##-----
 		##-----
@@ -78,5 +103,5 @@ for isub in [1, 2, 4, 5, 6, 7, 9, 10, 11, 13, 14, 18, 20, 21, 22, 23, 24, 25, 26
 	
 
 
-	plt.suptitle(['subject:', substr])
-	plt.show()
+	# plt.suptitle(['subject:', substr])
+	# plt.show()
